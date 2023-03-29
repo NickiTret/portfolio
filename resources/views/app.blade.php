6521796a93e4a3f1223595a822be6c5c2266b140
NickiTret <tretjakov.nickit@yandex.ru>
2023-03-29 15:25:49 +0300

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="yandex-verification" content="eb4921f405093029" />
     @vite('resources/js/app.js')
     @inertiaHead
     @routes
@@ -30,7 +31,6 @@
 
 <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
     <main>
-        <meta name="yandex-verification" content="eb4921f405093029" />
         @inertia
         <script src="/assets/lib/jquery/dist/jquery.js"></script>
         <script src="/assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
