71b0c7fabe77398d89d0699794ecc61f16b9096b
nicktre <tretjakov.nickit@yandex.ru>
2023-04-28 18:37:57 +0300

    fix

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -448,8 +448,8 @@
                 type: "POST", // Метод отправки
                 url: "tel.php", // Путь к PHP обработчику sendform.php
                 data: form_data,
-                // success: jQuery("#contactForm button").prop('disabled', true).text('Сообщение отправлено'),
-                success: console.log(url)
+                success: jQuery("#contactForm button").prop('disabled', true).text('Сообщение отправлено'),
+                // success: console.log(url)
 
             });
             $(this).find('input, textarea').prop('disabled', true);
